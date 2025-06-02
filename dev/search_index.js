5a9d4e42e8f1cd9f93206d6a81f8055b405d870c
Documenter.jl <documenter@juliadocs.github.io>
2025-06-02 03:16:30 +0000

    build based on 521670c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Win32","page":"Home","title":"Win32","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Win32.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
